8081928d0d712ba9b92251656133223a8ae8d39d
Ton <tonlove5233@gmail.com>
2026-05-11 22:37:49 +0700

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,16 @@
+@echo off
+title Project Updater
+
+echo ==========================
+echo Updating Project Source...
+echo ==========================
+
+git pull
+
+echo.
+echo ==========================
+echo Update Complete!
+echo Press any key to exit...
+echo ==========================
+
+pause >nul
